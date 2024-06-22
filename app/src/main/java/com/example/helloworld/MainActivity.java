7c9970bbcdb32a9756d8922c3faff3f3379a84fa
Maxwell Muthui Mwangi(Justmwangii) <muthuimaxwell67@gmail.com>
2024-06-23 00:07:48 +0300

    welcome to android development

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.helloworld;
+
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
+    /** Called when the activity restarts after stopping it. */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(msg, "The onRestart() event");
+    }
+}
